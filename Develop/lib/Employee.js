ad5643bb7a3f1d1bd79647d2eb725359ef6b4c06
yasha6081 <yasha6081@gmail.con>
2020-10-20 21:17:55 -0500

    all

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,14 @@
+// TODO: Write code to define and export the Employee class
+
+class Employee {
+      constructor(name, id , email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.getName()
+        this.getID()
+        this.getEmail()
+        this.getRole()
+
+      }
+    }
---
+++ Develop/lib/Engineer.js
@@ -0,0 +1,13 @@
+// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer {
+      constructor(name, id , email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.getName()
+        this.getID()
+        this.getEmail()
+        this.getRole()
+
+      }
+    }
